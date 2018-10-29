83dd664e14c50bab0a605400445a0539a345a1c0
Jeremy Greer <jeremy.greer@engageonq.com>
2018-10-29 13:44:52 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,72 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Scrollable extends Component {
+  constructor(props) {
+    super(props)
+    this.timer = setInterval(this.tick, 1000)
+    this.state = {
+      ticks: [],
+    }
+
+    this.messagesEnd = React.createRef()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.shouldScroll && !prevProps.shouldScroll) {
+      this.scrollToBottom()
+      this.props.onScrollComplete()
+    }
+  }
+
+  scrollToBottom = () => {
+    this.messagesEnd.current.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  tick = () => {
+    this.setState(prev => {
+      const ticks = [...prev.ticks]
+      ticks.push(Date.now())
+      return {
+        ticks,
+      }
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        I am scrollable
+        <ul>
+          { this.state.ticks.map(x => <li>{x}</li>) }
+        </ul>
+        <div ref={this.messagesEnd} />
+      </div>
+    )
+  }
+}
+
 class App extends Component {
+  state = {
+    scrollToBottom: false,
+  }
+
+  handleClick = () => {
+    this.setState({ scrollToBottom: true })
+  }
+
+  handleScroll = () => {
+    this.setState({ scrollToBottom: false })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button onClick={this.handleClick}>scroll it</button>
+        <Scrollable
+          shouldScroll={this.state.scrollToBottom}
+          onScrollComplete={this.handleScroll}
+        />
       </div>
     );
   }
